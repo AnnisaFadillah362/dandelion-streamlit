4842b003a4cf40ec55f99be59c7906b482f200ea
AnnisaFadillah362 <nischxn8@gmail.com>
2025-05-27 09:08:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Welcome To Dandelion")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "  beautiful flower called dandelion.)."
 )
