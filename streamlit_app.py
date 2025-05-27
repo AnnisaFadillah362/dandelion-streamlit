bf6f232bb53b42cbd12f19fbdc331ce3e6443a2e
AnnisaFadillah362 <nischxn8@gmail.com>
2025-05-27 09:47:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,12 @@ st.write(
 )
 
 st.image("2a5892c7d51a001a244a6139a36d0b97.jpg", width=200)
+st.title("Dandelions Information")
 
+st.header("Aplikasi Mengenai Bunga Dandelion")
+bunga = st.number_input("Tulis Bunga Kesukaanmu:", value=0, step=1)
+
+if (bunga 2)=0:
+ st.write(f"(bunga) dandelion adalah tumbuhan")
+else:
+ st.write(f"(bunga) lily adalah tumbuhan")
